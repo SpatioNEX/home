c7a3c81104bf92511acaf134968b9eae640928b6
Clementndome <clementndome@gmail.com>
2025-06-25 04:22:50 +0300

    created Service worern

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'spatioai-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/team.html',
+  '/styles.css',
+  '/scripts.js',
+  '/my-favicon/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  ); // Added missing parenthesis
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  ); // Added missing parenthesis
+});
